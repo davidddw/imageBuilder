f2db7c39d6a0831b4a7fdb9a43cbb0dff452c478
Dong Dawei <dawei@yunshan.net.cn>
2015-03-30 11:19:06 +0800

    init
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

---
+++ windows_2012_r2/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+
+: ${BUILD_VERSION:="v$(date +'%Y%m%d')"}
+: ${BUILD_NAME:="Windows_2012_R2"}
+export BUILD_NAME
+export BUILD_VERSION
+/usr/bin/packer-build build template.json
+
+cd builds
+tar zxf ${BUILD_NAME}-${BUILD_VERSION}.box
+
+qemu-img convert -O raw box.img box.raw
+vhd-util convert -s 0 -t 1 -i box.raw -o stagefixed.vhd
+vhd-util convert -s 1 -t 2 -i stagefixed.vhd -o ${BUILD_NAME}-${BUILD_VERSION}.vhd
+
+qemu-img convert -c -O qcow2 box.img ${BUILD_NAME}-${BUILD_VERSION}.qcow2
+bzip2 ${BUILD_NAME}-${BUILD_VERSION}.vhd
+rm -rf metadata.json Vagrantfile stagefixed.vhd.bak box.img
+echo "==> Generate files:"
+find . -type f -printf "==> %f\n"
+
+cd -
+
+echo "Done"
