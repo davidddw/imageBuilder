2ecd3bffe14a61cec9564631cf80cbf2613348e1
Dong Dawei <dawei@yunshan.net.cn>
2015-04-28 19:04:51 +0800

    add systemctl script for qemu-ga
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

--- CentOS_7.0_kvm_livecloud/scripts/custom.sh
+++ CentOS_7.0_kvm_livecloud/scripts/custom.sh
@@ -1,11 +1,32 @@
 # add custom script in here
 
+cat <<'EOF' > /usr/lib/systemd/system/qemu-guest-agent.service
+[Unit]
+Description=QEMU Guest Agent
+BindsTo=dev-virtio\x2dports-org.qemu.guest_agent.0.device
+After=dev-virtio\x2dports-org.qemu.guest_agent.0.device
 
+[Service]
+UMask=0077
+EnvironmentFile=/etc/sysconfig/qemu-ga
+ExecStart=/usr/bin/qemu-ga \
+  --method=virtio-serial \
+  --path=/dev/virtio-ports/org.qemu.guest_agent.0 \
+  --blacklist=${BLACKLIST_RPC} \
+  -F${FSFREEZE_HOOK_PATHNAME}
+StandardError=syslog
+Restart=always
+RestartSec=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+mkdir -p /usr/local/var/run/
 # wget vm_init
 cd /etc/ && wget http://172.16.39.10/09_config/vm_init.sh && chmod +x vm_init.sh
 # wget qemu_ga
 cd /usr/bin && wget http://172.16.39.10/09_config/qga/qemu-ga.el7 && mv qemu-ga.el7 qemu-ga && chmod +x qemu-ga
-
 
 # enable tty console
 sed -i 's#quiet#quiet console=ttyS0#' /etc/default/grub
--- Debian_8.0_kvm_livecloud/scripts/custom_kvm.sh
+++ Debian_8.0_kvm_livecloud/scripts/custom_kvm.sh
@@ -1,114 +1,26 @@
 # add custom script in here
 
 # add respawn script
-cat <<'EOF' > /etc/init.d/qemu-guest-agent 
-#! /bin/sh
-### BEGIN INIT INFO
-# Provides:          qemu-guest-agent
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: QEMU Guest Agent startup script
-# Description:       Start the QEMU Guest Agent if we're running
-#                    in a QEMU virtual machine
-### END INIT INFO
+cat <<'EOF' > /usr/lib/systemd/system/qemu-guest-agent.service
+[Unit]
+Description=QEMU Guest Agent
+BindsTo=dev-virtio\x2dports-org.qemu.guest_agent.0.device
+After=dev-virtio\x2dports-org.qemu.guest_agent.0.device
 
-# Author: Michael Tokarev <mjt@tls.msk.ru>
+[Service]
+UMask=0077
+EnvironmentFile=/etc/sysconfig/qemu-ga
+ExecStart=/usr/bin/qemu-ga \
+  --method=virtio-serial \
+  --path=/dev/virtio-ports/org.qemu.guest_agent.0 \
+  --blacklist=${BLACKLIST_RPC} \
+  -F${FSFREEZE_HOOK_PATHNAME}
+StandardError=syslog
+Restart=always
+RestartSec=0
 
-PATH=/sbin:/usr/sbin:/bin:/usr/bin
-DESC="QEMU Guest Agent"
-NAME=qemu-ga
-DAEMON=/usr/sbin/$NAME
-PIDFILE=/var/run/$NAME.pid
-
-# config
-DAEMON_ARGS=""
-# default transport
-TRANSPORT=virtio-serial:/dev/virtio-ports/org.qemu.guest_agent.0
-
-# Exit if the package is not installed
-[ -x "$DAEMON" ] || exit 0
-
-# Read configuration variable file if it is present
-[ -r /etc/default/qemu-guest-agent ] && . /etc/default/qemu-guest-agent
-
-# Load the VERBOSE setting and other rcS variables
-. /lib/init/vars.sh
-. /lib/lsb/init-functions
-
-do_check_transport() {
-    method=${TRANSPORT%%:*}; path=${TRANSPORT#*:}
-    case "$method" in
-        virtio-serial | isa-serial)
-        if [ ! -e "$path" ]; then
-            log_warning_msg "$NAME: transport endpoint not found, not starting"
-            return 1
-        fi
-        ;;
-    esac
-}
-
-do_start()
-{
-    start-stop-daemon -Sq -p $PIDFILE -x $DAEMON --test > /dev/null \
-        || return 1
-    start-stop-daemon -Sq -p $PIDFILE -x $DAEMON -- --daemonize \
-        $DAEMON_ARGS -m "$method" -p "$path" \
-        || return 2
-}
-
-do_stop()
-{
-    start-stop-daemon -Kq --retry=TERM/30/KILL/5 -p $PIDFILE --name $NAME
-}
-
-case "$1" in
-  start)
-    do_check_transport || exit 0
-    [ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC" $NAME
-    do_start
-    case "$?" in
-        0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-        2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
-    esac
-    ;;
-  stop)
-    [ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" $NAME
-    do_stop
-    case "$?" in
-        0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-        2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
-    esac
-    ;;
-  status)
-    status_of_proc "$DAEMON" $NAME && exit 0 || exit $?
-    ;;
-  restart|force-reload) # we do not support reload
-    do_check_transport || exit 0
-    log_daemon_msg "Restarting $DESC" $NAME
-    do_stop
-    case "$?" in
-        0|1)
-            do_start
-            case "$?" in
-                0) log_end_msg 0 ;;
-                1) log_end_msg 1 ;; # Old process is still running
-                *) log_end_msg 1 ;; # Failed to start
-            esac
-            ;;
-        *)
-            # Failed to stop
-            log_end_msg 1
-            ;;
-    esac
-    ;;
-  *)
-    echo "Usage: /etc/init.d/qemu-guest-agent {start|stop|status|restart|force-reload}" >&2
-        exit 3
-        ;;
-esac
-:
+[Install]
+WantedBy=multi-user.target
 EOF
 
 chmod +x /etc/init.d/qemu-guest-agent && chkconfig qemu-guest-agent on
