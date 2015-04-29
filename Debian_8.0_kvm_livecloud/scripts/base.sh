cf912b0c8d363837f9963828d9bedab2edaf0008
Dong Dawei <dawei@yunshan.net.cn>
2015-04-29 20:04:37 +0800

    fix debian8 cannot connect openssh and qemu-ga.service
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

--- Debian_8.0_kvm_livecloud/scripts/base.sh
+++ Debian_8.0_kvm_livecloud/scripts/base.sh
@@ -1,15 +1,8 @@
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
-sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
-
 cat <<'EOF' > /etc/apt/sources.list
 deb http://172.16.39.42/debian jessie main
 EOF
 apt-get update
 apt-get install -y --force-yes chkconfig libglib2.0-0 curl 
-
-sed -i 's#PermitRootLogin.*#PermitRootLogin yes#' /etc/ssh/sshd_config 
-echo "UseDNS no" >> /etc/ssh/sshd_config
-systemctl restart ssh.service
 
 cat <<'EOF' > /root/.bashrc
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
--- Debian_8.0_kvm_livecloud/scripts/custom_kvm.sh
+++ Debian_8.0_kvm_livecloud/scripts/custom_kvm.sh
@@ -1,7 +1,7 @@
 # add custom script in here
 
 # add respawn script
-cat <<'EOF' > /usr/lib/systemd/system/qemu-guest-agent.service
+cat <<'EOF' > /lib/systemd/system/qemu-guest-agent.service
 [Unit]
 Description=QEMU Guest Agent
 BindsTo=dev-virtio\x2dports-org.qemu.guest_agent.0.device
@@ -9,12 +9,10 @@ After=dev-virtio\x2dports-org.qemu.guest_agent.0.device
 
 [Service]
 UMask=0077
-EnvironmentFile=/etc/sysconfig/qemu-ga
 ExecStart=/usr/bin/qemu-ga \
   --method=virtio-serial \
   --path=/dev/virtio-ports/org.qemu.guest_agent.0 \
-  --blacklist=${BLACKLIST_RPC} \
-  -F${FSFREEZE_HOOK_PATHNAME}
+  --blacklist=guest-file-open,guest-file-close,guest-file-read,guest-file-write,guest-file-seek,guest-file-flush
 StandardError=syslog
 Restart=always
 RestartSec=0
@@ -23,13 +21,13 @@ RestartSec=0
 WantedBy=multi-user.target
 EOF
 
-chmod +x /etc/init.d/qemu-guest-agent && chkconfig qemu-guest-agent on
+systemctl enable qemu-guest-agent.service
 mkdir -p /usr/local/var/run/
 
 # wget vm_init
 cd /etc/ && wget http://172.16.39.10/09_config/vm_init.sh && chmod +x vm_init.sh
 # wget qemu_ga
-cd /usr/sbin && wget http://172.16.39.10/09_config/qga/qemu-ga.deb8 && mv qemu-ga.deb8 qemu-ga && chmod +x qemu-ga
+cd /usr/bin && wget http://172.16.39.10/09_config/qga/qemu-ga.deb8 && mv qemu-ga.deb8 qemu-ga && chmod +x qemu-ga
 
 # enable tty console
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
