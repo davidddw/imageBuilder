734c31981086ec89ca68cddd7c5b019daebeffb1
Dong Dawei <dawei@yunshan.net.cn>
2015-04-30 18:59:14 +0800

    fix ubuntu bug
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

--- ubuntu_12.04_kvm/scripts/base.sh
+++ ubuntu_12.04_kvm/scripts/base.sh
@@ -1,4 +1,26 @@
 
+cat <<'EOF' > /etc/apt/sources.list
+deb http://mirrors.ustc.edu.cn/ubuntu/ precise-updates main restricted
+deb-src http://mirrors.ustc.edu.cn/ubuntu/ precise-updates main restricted
+deb http://mirrors.ustc.edu.cn/ubuntu/ precise universe
+deb-src http://mirrors.ustc.edu.cn/ubuntu/ precise universe
+deb http://mirrors.ustc.edu.cn/ubuntu/ precise-updates universe
+deb-src http://mirrors.ustc.edu.cn/ubuntu/ precise-updates universe
+deb http://mirrors.ustc.edu.cn/ubuntu/ precise multiverse
+deb-src http://mirrors.ustc.edu.cn/ubuntu/ precise multiverse
+deb http://mirrors.ustc.edu.cn/ubuntu/ precise-updates multiverse
+deb-src http://mirrors.ustc.edu.cn/ubuntu/ precise-updates multiverse
+deb http://mirrors.ustc.edu.cn/ubuntu/ precise-backports main restricted universe multiverse
+deb-src http://mirrors.ustc.edu.cn/ubuntu/ precise-backports main restricted universe multiverse
+
+deb http://security.ubuntu.com/ubuntu precise-security main restricted
+deb-src http://security.ubuntu.com/ubuntu precise-security main restricted
+deb http://security.ubuntu.com/ubuntu precise-security universe
+deb-src http://security.ubuntu.com/ubuntu precise-security universe
+deb http://security.ubuntu.com/ubuntu precise-security multiverse
+deb-src http://security.ubuntu.com/ubuntu precise-security multiverse
+EOF
+
 apt-get update
 apt-get install -y --force-yes chkconfig libglib2.0-0 curl 
 
--- ubuntu_12.04_kvm/scripts/custom_kvm.sh
+++ ubuntu_12.04_kvm/scripts/custom_kvm.sh
@@ -6,4 +6,4 @@ sed -i -e 's#GRUB_CMDLINE_LINUX=.*$#GRUB_CMDLINE_LINUX="text console=tty0 consol
 -e 's/#GRUB_TERMINAL=console/GRUB_TERMINAL=console/' \
 -e 's#GRUB_CMDLINE_LINUX_DEFAULT="quiet"#GRUB_CMDLINE_LINUX_DEFAULT=""#' /etc/default/grub
 update-grub
-sed -i 's@#T0.*@T0:23:respawn:/sbin/getty -L ttyS0 115200 vt100@' /etc/inittab
+#sed -i 's@#T0.*@T0:23:respawn:/sbin/getty -L ttyS0 115200 vt100@' /etc/inittab
