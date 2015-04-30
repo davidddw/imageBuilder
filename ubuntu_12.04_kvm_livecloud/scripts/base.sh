49e009dbe390d5b542da26a975b88c60d8d57278
Dong Dawei <dawei@yunshan.net.cn>
2015-04-30 13:29:37 +0800

    change ubuntu 12.04 scripts
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

--- ubuntu_12.04_kvm_livecloud/scripts/base.sh
+++ ubuntu_12.04_kvm_livecloud/scripts/base.sh
@@ -1,4 +1,4 @@
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
-sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
+apt-get update
+apt-get install -y --force-yes chkconfig libglib2.0-0 curl 
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
