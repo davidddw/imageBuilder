7f940b672f7562169b9a7d7607e2e44e04a25fa9
Dong Dawei <dawei@yunshan.net.cn>
2015-05-01 17:00:55 +0800

    fix ubuntu cannot login
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

--- ubuntu_14.04_kvm/scripts/custom_kvm.sh
+++ ubuntu_14.04_kvm/scripts/custom_kvm.sh
@@ -2,20 +2,24 @@
 
 # enable tty console
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
-sed -i -e 's#GRUB_CMDLINE_LINUX=.*$#GRUB_CMDLINE_LINUX="console=tty0 console=ttyS0,115200n8"#' \
+sed -i -e 's#GRUB_CMDLINE_LINUX=.*$#GRUB_CMDLINE_LINUX="text console=tty1 console=ttyS0,115200n8"#' \
+-e 's/#GRUB_TERMINAL=console/GRUB_TERMINAL=console/' \
 -e 's#GRUB_CMDLINE_LINUX_DEFAULT="quiet"#GRUB_CMDLINE_LINUX_DEFAULT=""#' /etc/default/grub
 update-grub
 
 cat <<'EOF' > /etc/init/ttyS0.conf
-/etc/init/ttyS0.conf 
 # ttyS0 - getty
 #
-# This service maintains a getty on ttyS0 from the point the system is
+# This service maintains a getty on tty1 from the point the system is
 # started until it is shut down again.
 
-start on stopped rc RUNLEVEL=[2345]
+start on stopped rc RUNLEVEL=[2345] and (
+            not-container or
+            container CONTAINER=lxc or
+            container CONTAINER=lxc-libvirt)
+
 stop on runlevel [!2345]
 
 respawn
-exec /sbin/getty -L 115200 ttyS0 vt102
+exec /sbin/getty -8 115200 ttyS0
 EOF
