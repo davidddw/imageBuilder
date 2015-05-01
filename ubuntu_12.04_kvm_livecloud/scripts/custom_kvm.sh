35e3bd4af61b0b842325e68c85ebf214c08d6dfa
Dong Dawei <dawei@yunshan.net.cn>
2015-05-01 15:51:39 +0800

    fix ubuntu no console bug
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

--- ubuntu_12.04_kvm_livecloud/scripts/custom_kvm.sh
+++ ubuntu_12.04_kvm_livecloud/scripts/custom_kvm.sh
@@ -2,8 +2,20 @@
 
 # enable tty console
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
-sed -i -e 's#GRUB_CMDLINE_LINUX=.*$#GRUB_CMDLINE_LINUX="text console=tty0 console=ttyS0,115200n8"#' \
--e 's/#GRUB_TERMINAL=console/GRUB_TERMINAL=console/' \
+sed -i -e 's#GRUB_CMDLINE_LINUX=.*$#GRUB_CMDLINE_LINUX="console=tty0 console=ttyS0,115200n8"#' \
 -e 's#GRUB_CMDLINE_LINUX_DEFAULT="quiet"#GRUB_CMDLINE_LINUX_DEFAULT=""#' /etc/default/grub
 update-grub
-sed -i 's@#T0.*@T0:23:respawn:/sbin/getty -L ttyS0 115200 vt100@' /etc/inittab
+
+cat <<'EOF' > /etc/init/ttyS0.conf
+/etc/init/ttyS0.conf 
+# ttyS0 - getty
+#
+# This service maintains a getty on ttyS0 from the point the system is
+# started until it is shut down again.
+
+start on stopped rc RUNLEVEL=[2345]
+stop on runlevel [!2345]
+
+respawn
+exec /sbin/getty -L 115200 ttyS0 vt102
+EOF
