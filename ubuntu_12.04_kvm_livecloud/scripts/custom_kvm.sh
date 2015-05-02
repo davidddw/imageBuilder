896ad338b7ed8f7e92e76c886af6f6e62663acd6
Dong Dawei <dawei@yunshan.net.cn>
2015-05-02 19:46:59 +0800

    fix ubuntu qemu-ga bug
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

--- ubuntu_12.04_kvm_livecloud/scripts/custom_kvm.sh
+++ ubuntu_12.04_kvm_livecloud/scripts/custom_kvm.sh
@@ -1,5 +1,30 @@
 # add custom script in here
 
+# add respawn script
+cat <<'EOF' > /etc/init/qemu-ga.conf 
+# qemu-ga
+start on runlevel [2345]
+stop on runlevel [016]
+
+respawn
+env TRANSPORT_METHOD="virtio-serial"
+env DEVPATH="/dev/virtio-ports/org.qemu.guest_agent.0"
+env LOGFILE="/var/log/qemu-ga/qemu-ga.log"
+env PIDFILE="/var/run/qemu-ga.pid"
+env BLACKLIST_RPC="guest-file-open guest-file-close guest-file-read guest-file-write guest-file-seek guest-file-flush"
+
+pre-start script
+    [ -d /var/log/qemu-ga ] || mkdir -p /var/log/qemu-ga
+    [ -d /usr/local/var/run/ ] || mkdir -p /usr/local/var/run/
+end script
+exec /usr/bin/qemu-ga --method $TRANSPORT_METHOD --path $DEVPATH --logfile $LOGFILE --pidfile $PIDFILE --blacklist $BLACKLIST_RPC
+EOF
+
+# wget vm_init
+cd /etc/ && wget http://172.16.39.10/09_config/vm_init.sh && chmod +x vm_init.sh
+# wget qemu_ga
+cd /usr/bin && wget http://172.16.39.10/09_config/qga/qemu-ga.ubuntu12 && mv qemu-ga.ubuntu12 qemu-ga && chmod +x qemu-ga
+
 # enable tty console
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
 sed -i -e 's#GRUB_CMDLINE_LINUX=.*$#GRUB_CMDLINE_LINUX="text console=tty1 console=ttyS0,115200n8"#' \
