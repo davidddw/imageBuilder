c77ceccdcad9fac671164c5e4b8d806b737d47b5
Dong Dawei <dawei@yunshan.net.cn>
2015-06-03 11:11:59 +0800

    fix opensuse 12.3 repo
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

--- opensuse_13.2_kvm/scripts/base.sh
+++ opensuse_13.2_kvm/scripts/base.sh
@@ -1,8 +1,8 @@
 # Base install
 
 rm -f /etc/zypp/repos.d/openSUSE-13.2-0.repo
-rpm --import http://download.opensuse.org/distribution/13.2/repo/oss/gpg-pubkey-3dbdc284-53674dd4.asc
-rpm --import http://download.opensuse.org/distribution/13.2/repo/oss/gpg-pubkey-307e3d54-4be01a65.asc
+rpm --import http://mirror.bit.edu.cn/opensuse/distribution/12.3/repo/oss/gpg-pubkey-307e3d54-4be01a65.asc
+rpm --import http://mirror.bit.edu.cn/opensuse/distribution/12.3/repo/oss/gpg-pubkey-3dbdc284-53674dd4.asc
 zypper ar http://mirror.bit.edu.cn/opensuse/distribution/13.2/repo/oss/ opensuse-13.2-oss
 zypper ar http://mirror.bit.edu.cn/opensuse/distribution/13.2/repo/non-oss/ opensuse-13.2-non-oss
 zypper refresh
