a7fc8281cc3482c498ef369c4c6ac258bf458b1c
Dong Dawei <dawei@yunshan.net.cn>
2015-06-03 10:57:03 +0800

    add opensuse 12.3 support
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

---
+++ opensuse_12.3_kvm_livecloud/scripts/base.sh
@@ -0,0 +1,15 @@
+# Base install
+
+rm -f /etc/zypp/repos.d/openSUSE-13.2-0.repo
+rpm --import http://download.opensuse.org/distribution/13.2/repo/oss/gpg-pubkey-3dbdc284-53674dd4.asc
+rpm --import http://download.opensuse.org/distribution/13.2/repo/oss/gpg-pubkey-307e3d54-4be01a65.asc
+zypper ar http://mirror.bit.edu.cn/opensuse/distribution/13.2/repo/oss/ opensuse-13.2-oss
+zypper ar http://mirror.bit.edu.cn/opensuse/distribution/13.2/repo/non-oss/ opensuse-13.2-non-oss
+zypper refresh
+zypper -n install libgthread-2_0-0
+systemctl enable sshd
+systemctl enable wicked
+echo "UseDNS no" >> /etc/ssh/sshd_config
+echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
+
+sed -i "s/nameserver .*/nameserver 8.8.8.8/" /etc/resolv.conf 
