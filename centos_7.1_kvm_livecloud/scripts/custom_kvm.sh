67292a4c927e7e0056dca3e3e8aa041976902b5f
Dong Dawei <dawei@yunshan.net.cn>
2015-06-10 15:05:37 +0800

    rm centos7 network config file and add debian 8.1 executive attribution
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

--- centos_7.1_kvm_livecloud/scripts/custom_kvm.sh
+++ centos_7.1_kvm_livecloud/scripts/custom_kvm.sh
@@ -30,3 +30,7 @@ cd /usr/bin && wget http://172.16.39.10/09_config/qga/qemu-ga.el7 && mv qemu-ga.
 # enable tty console
 sed -i 's#quiet#quiet console=ttyS0#' /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
+
+# remove ip and mac address
+rm -fr /etc/udev/rules.d/70-persistent-net.rules
+rm -fr /etc/sysconfig/network-scripts/ifcfg-eth0
