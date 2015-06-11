86e2429783ca29ed49cb6397af961d56a18da0eb
Dong Dawei <dawei@yunshan.net.cn>
2015-06-11 11:23:06 +0800

    add some packages to debian
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

--- debian_8.1_kvm_livecloud/scripts/base.sh
+++ debian_8.1_kvm_livecloud/scripts/base.sh
@@ -3,9 +3,13 @@ deb http://ftp.cn.debian.org/debian/ jessie main contrib non-free
 deb-src http://ftp.cn.debian.org/debian/ jessie main contrib non-free
 deb http://ftp.cn.debian.org/debian-security/ jessie/updates main
 deb-src http://ftp.cn.debian.org/debian-security/ jessie/updates main
+deb http://ftp.cn.debian.org/debian/ sid main non-free contrib
 EOF
 apt-get update
 apt-get install -y --force-yes chkconfig libglib2.0-0 curl 
+apt-get install -y --force-yes stress tcpdump hping3 netperf iperf nmap mtr \
+redis-server openjdk-7-jdk apache2-utils parted gdisk nginx sshpass
+apt-get -t sid install -y --force-yes linux-image-amd64 
 
 cat <<'EOF' > /root/.bashrc
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
