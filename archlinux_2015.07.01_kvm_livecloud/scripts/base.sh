1f4ba3cea9594fbd1f93c1a45695e6b752f7d909
Dong Dawei <dawei@yunshan.net.cn>
2015-07-15 13:50:33 +0800

    fix ubuntu 15.04 cannot install grub
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

---
+++ archlinux_2015.07.01_kvm_livecloud/scripts/base.sh
@@ -0,0 +1,35 @@
+# Base install
+
+
+# Fix slow DNS lookups with VirtualBox's DNS proxy:
+# https://github.com/mitchellh/vagrant/issues/1172#issuecomment-9438465
+echo 'options single-request-reopen' >> /etc/resolv.conf
+sed -i "s/nameserver .*/nameserver 8.8.8.8/" /etc/resolv.conf 
+
+cat <<'EOF' > /etc/yum.repos.d/custom.repo
+# yum --disablerepo=\* --enablerepo=centos6,epel6 [command]
+
+[centos6]
+name=CentOS-$releasever - Media
+baseurl=http://172.16.39.10/00_mirrors/centos6/centos6.6
+gpgcheck=0
+enabled=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
+
+[epel6]
+name=CentOS-$releasever - Custom
+baseurl=http://172.16.39.10/00_mirrors/centos6/epel6
+gpgcheck=0
+enabled=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
+
+EOF
+
+yum --disablerepo=\* --enablerepo=centos6,epel6 -y install vim openssh-clients \
+wget stress tcpdump hping3 netperf iperf nmap mtr redis java-1.7.0-openjdk \
+httpd-tools parted gdisk nginx sshpass
+
+# Make ssh faster by not waiting on DNS
+echo "UseDNS no" >> /etc/ssh/sshd_config
+
+
