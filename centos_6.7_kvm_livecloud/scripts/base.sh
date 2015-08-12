1ecb4c6cc84e546d131d61e78a1f43011848800b
Dong Dawei <dawei@yunshan.net.cn>
2015-08-12 21:12:19 +0800

    add CentOS 6.7 support

---
+++ centos_6.7_kvm_livecloud/scripts/base.sh
@@ -0,0 +1,31 @@
+# Base install
+
+
+# Fix slow DNS lookups with VirtualBox's DNS proxy:
+# https://github.com/mitchellh/vagrant/issues/1172#issuecomment-9438465
+echo 'options single-request-reopen' >> /etc/resolv.conf
+
+cat <<'EOF' > /etc/yum.repos.d/custom.repo
+# yum --disablerepo=\* --enablerepo=centos6,epel6 [command]
+
+[centos6]
+name=CentOS-$releasever - Media
+baseurl=http://172.16.39.10/00_mirrors/centos6/centos6.6
+gpgcheck=0
+enabled=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
+
+[epel6]
+name=CentOS-$releasever - Custom
+baseurl=http://172.16.39.10/00_mirrors/centos6/epel6
+gpgcheck=0
+enabled=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
+
+EOF
+ 
+yum --disablerepo=\* --enablerepo=centos6,epel6 -y install vim openssh-clients \
+wget 
+
+# Make ssh faster by not waiting on DNS
+echo "UseDNS no" >> /etc/ssh/sshd_config
