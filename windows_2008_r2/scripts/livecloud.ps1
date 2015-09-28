95d0e4b20983cd74572fae2f000ff6afb4c7f360
Dong Dawei <dawei@yunshan.net.cn>
2015-09-28 18:15:48 +0800

    add vagent to auto startscript
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

--- windows_2008_r2/scripts/livecloud.ps1
+++ windows_2008_r2/scripts/livecloud.ps1
@@ -5,18 +5,26 @@ if ($UserExist) {
 	$server.delete("user", "livecloud")
 }
 
+$7z_download_url = "http://172.16.2.254/Packer/7z1507-x64.exe"
+if (!(Test-Path "C:\Program Files\7-Zip")) {
+    Write-Host "Downloading $7z_download_url"
+    (New-Object System.Net.WebClient).DownloadFile($7z_download_url, "C:\Windows\Temp\7z1507-x64.exe")
+    Start-Process "C:\Windows\Temp\7z1507-x64.exe" "/S" -NoNewWindow -Wait
+}
+
 $python_download_url = "http://172.16.2.254/Packer/python-2.7.8.amd64.msi"
-if (!(Test-Path "C:\python27" )) {
+if (!(Test-Path "C:\Program Files\python" )) {
     Write-Host "Downloading $python_download_url"
     $msiFile = "C:\Windows\Temp\python-2.7.8.amd64.msi"
+    $targetdit = "C:\Program Files\python27"
     ( New-Object System.Net.WebClient).DownloadFile( $python_download_url , "$msiFile" )
     $arguments = @(
-		"/i"
-		"`"$msiFile`""
-	    "/qn"
-		"/norestart"
-		"ALLUSERS=1"
-		"TARGETDIR=C:\python27"
+        "/i"
+        "`"$msiFile`""
+        "/qn"
+        "/norestart"
+        "ALLUSERS=1"
+        "TARGETDIR=`"$targetdit`""
 	)
 	Write-Host "Installing $msiFile....."
 	$process = Start-Process -FilePath msiexec.exe -ArgumentList $arguments -Wait -PassThru
@@ -26,18 +34,16 @@ if (!(Test-Path "C:\python27" )) {
     	Write-Host "installer exit code  $($process.ExitCode) for file  $($msifile)"
 	}
 }
-[Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\python27\;C:\python27\Scripts\", "User")
+[Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\Program Files\python\;C:\Program Files\python\Scripts\", "User")
 
-$virtio_download_url = "http://172.16.2.254/Packer/virtiodriver.zip"
+$virtio_download_url = "http://172.16.2.254/Packer/virtiodriver.tar.gz"
 if (!(Test-Path "C:\Windows\virtiodriver" )) {
     Write-Host "Downloading $virtio_download_url"
-    $driverFile = "C:\Windows\Temp\virtiodriver.zip"
+    $driverFile = "C:\Windows\Temp\virtiodriver.tar.gz"
     ( New-Object System.Net.WebClient).DownloadFile( $virtio_download_url, "$driverFile" )
-    $shell = new-object -com shell.application
-    $zip = $shell.NameSpace("$driverFile")
-    foreach($item in $zip.items()) {
-        $shell.Namespace("C:\Windows" ).copyhere($item)
-    }
+    $sevenZip = "C:\Program Files\7-zip\7z.exe"
+    &$sevenZip e -y -oC:\Windows\Temp $driverFile
+    &$sevenZip x -y C:\Windows\Temp\virtiodriver.tar -oC:\Windows
 }
 
 $Host.UI.RawUI.WindowTitle = "Installing VirtIO certificate..."
@@ -79,9 +85,34 @@ if (!(Test-Path "C:\Program Files\qemu-ga" )) {
 	Start-Service "QEMU Guest Agent"
 }
 
-$rar_download_url = "http://172.16.2.254/Packer/WinRAR5.21_x64sc.exe"
-if (!(Test-Path "C:\Program Files\WinRAR")) {
-    Write-Host "Downloading $rar_download_url"
-    (New-Object System.Net.WebClient).DownloadFile($rar_download_url, "C:\Windows\Temp\winrar.exe")
-    Start-Process "C:\Windows\Temp\winrar.exe" "/S" -NoNewWindow -Wait
+secedit /export /cfg c:\secpol.cfg
+$config_file = Get-Content 'C:\secpol.cfg'
+$config_file = $config_file -replace 'PasswordComplexity = 1', 'PasswordComplexity = 0'
+$config_file = $config_file -replace 'MinimumPasswordLength = 1', 'MinimumPasswordLength = 0'
+Set-Content 'c:\secpol.cfg' $config_file
+secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
+rm -force c:\secpol.cfg -confirm:$false
+
+$vagent_download_url = "http://172.16.2.254/Packer/vagent.tar.gz"
+if (!(Test-Path "C:\Windows\vagent" )) {
+    Write-Host "Downloading $vagent_download_url"
+    $driverFile = "C:\Windows\Temp\vagent.tar.gz"
+    ( New-Object System.Net.WebClient).DownloadFile( $vagent_download_url, "$driverFile" )
+    $sevenZip = "C:\Program Files\7-zip\7z.exe"
+    &$sevenZip e -y -oC:\Windows\Temp $driverFile
+    &$sevenZip x -y C:\Windows\Temp\vagent.tar -oC:\Windows
+}
+
+$srvstart_download_url = "http://172.16.2.254/Packer/srvstart.tar.gz"
+if (!(Test-Path "C:\Windows\srvstart" )) {
+    Write-Host "Downloading $srvstart_download_url"
+    $driverFile = "C:\Windows\Temp\srvstart.tar.gz"
+    ( New-Object System.Net.WebClient).DownloadFile( $srvstart_download_url, "$driverFile" )
+    $sevenZip = "C:\Program Files\7-zip\7z.exe"
+    &$sevenZip e -y -oC:\Windows\Temp $driverFile
+    &$sevenZip x -y C:\Windows\Temp\srvstart.tar -oC:\Windows
+    $srvstart = "C:\Windows\srvstart\srvstart.exe"
+    &$srvstart install vagent -c C:\Windows\srvstart\srvstart.ini
+    Set-Service -Name "vagent" -StartupType Automatic
+    net start vagent
 }
