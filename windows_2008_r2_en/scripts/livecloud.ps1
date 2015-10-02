d4a33f900f423289c7b0ed0f8d843320113edf18
Dong Dawei <dawei@yunshan.net.cn>
2015-10-02 15:57:31 +0800

    add windows2008 english version support.
    Signed-off-by: Dong Dawei <dawei@yunshan.net.cn>

---
+++ windows_2008_r2_en/scripts/livecloud.ps1
@@ -0,0 +1,117 @@
+Write-Host "Delete livecloud User"
+$UserExist = [ADSI]::Exists("WinNT://livecloud-2008/livecloud")
+if ($UserExist) {
+    [ADSI]$server="WinNT://livecloud-2008"
+    $server.delete("user", "livecloud")
+}
+
+$7z_download_url = "http://172.16.2.254/Packer/7z1507-x64.exe"
+if (!(Test-Path "C:\Program Files\7-Zip")) {
+    Write-Host "Downloading $7z_download_url"
+    (New-Object System.Net.WebClient).DownloadFile($7z_download_url, "C:\Windows\Temp\7z1507-x64.exe")
+    Start-Process "C:\Windows\Temp\7z1507-x64.exe" "/S" -NoNewWindow -Wait
+}
+
+$python_download_url = "http://172.16.2.254/Packer/python-2.7.8.amd64.msi"
+if (!(Test-Path "C:\Program Files\python" )) {
+    Write-Host "Downloading $python_download_url"
+    $msiFile = "C:\Windows\Temp\python-2.7.8.amd64.msi"
+    $targetdit = "C:\Program Files\python"
+    ( New-Object System.Net.WebClient).DownloadFile( $python_download_url , "$msiFile" )
+    $arguments = @(
+        "/i"
+        "`"$msiFile`""
+        "/qn"
+        "/norestart"
+        "ALLUSERS=1"
+        "TARGETDIR=`"$targetdit`""
+    )
+    Write-Host "Installing $msiFile....."
+    $process = Start-Process -FilePath msiexec.exe -ArgumentList $arguments -Wait -PassThru
+    if ($process.ExitCode -eq 0){
+        Write-Host "$msiFile has been successfully installed"
+    } else {
+        Write-Host "installer exit code  $($process.ExitCode) for file  $($msifile)"
+    }
+}
+[Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\Program Files\python\;C:\Program Files\python\Scripts\", "User")
+
+$virtio_download_url = "http://172.16.2.254/Packer/virtiodriver2008R2.tar.gz"
+if (!(Test-Path "C:\Windows\virtiodriver" )) {
+    Write-Host "Downloading $virtio_download_url"
+    $driverFile = "C:\Windows\Temp\virtiodriver.tar.gz"
+    ( New-Object System.Net.WebClient).DownloadFile( $virtio_download_url, "$driverFile" )
+    $sevenZip = "C:\Program Files\7-zip\7z.exe"
+    &$sevenZip e -y -oC:\Windows\Temp $driverFile
+    &$sevenZip x -y C:\Windows\Temp\virtiodriver.tar -oC:\Windows
+}
+
+$Host.UI.RawUI.WindowTitle = "Installing VirtIO certificate..."
+$virtioCertPath = "C:\Windows\virtiodriver\VirtIO.cer"
+$virtioDriversPath = "C:\Windows\virtiodriver"
+$cacert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($virtioCertPath)
+$castore = New-Object System.Security.Cryptography.X509Certificates.X509Store([System.Security.Cryptography.X509Certificates.StoreName]::TrustedPublisher,`
+           [System.Security.Cryptography.X509Certificates.StoreLocation]::LocalMachine)
+$castore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
+$castore.Add($cacert)
+Write-Host "Installing VirtIO drivers from: $virtioDriversPath"
+$process = Start-process -Wait -PassThru pnputil "-i -a C:\Windows\virtiodriver\*.inf"
+if ($process.ExitCode -eq 0){
+    Write-Host "VirtIO has been successfully installed"
+} else {
+    Write-Host "InstallVirtIO failed"
+}
+
+$vagent_download_url = "http://172.16.2.254/Packer/vagent.tar.gz"
+if (!(Test-Path "C:\Windows\vagent" )) {
+    Write-Host "Downloading $vagent_download_url"
+    $driverFile = "C:\Windows\Temp\vagent.tar.gz"
+    ( New-Object System.Net.WebClient).DownloadFile( $vagent_download_url, "$driverFile" )
+    $sevenZip = "C:\Program Files\7-zip\7z.exe"
+    &$sevenZip e -y -oC:\Windows\Temp $driverFile
+    &$sevenZip x -y C:\Windows\Temp\vagent.tar -oC:\Windows
+}
+
+$srvstart_download_url = "http://172.16.2.254/Packer/srvstart.tar.gz"
+if (!(Test-Path "C:\Windows\srvstart" )) {
+    Write-Host "Downloading $srvstart_download_url"
+    $driverFile = "C:\Windows\Temp\srvstart.tar.gz"
+    ( New-Object System.Net.WebClient).DownloadFile( $srvstart_download_url, "$driverFile" )
+    $sevenZip = "C:\Program Files\7-zip\7z.exe"
+    &$sevenZip e -y -oC:\Windows\Temp $driverFile
+    &$sevenZip x -y C:\Windows\Temp\srvstart.tar -oC:\Windows
+    $srvstart = "C:\Windows\srvstart\srvstart.exe"
+    &$srvstart install vagent -c C:\Windows\srvstart\srvstart.ini
+    Set-Service -Name "vagent" -StartupType Automatic -description "LiveCloud Agent for VM"
+    net start vagent
+    
+    # configure firewall
+    Write-Host "Configuring firewall"
+    netsh advfirewall firewall add rule name="vagent" dir=in action=allow service=vagent enable=yes
+    netsh advfirewall firewall add rule name="vagent" dir=in action=allow program="C:\Windows\vagent\vagent.py" enable=yes
+    netsh advfirewall firewall add rule name="vagent" dir=in action=allow protocol=TCP localport=12345
+}
+
+$qga_download_url = "http://172.16.2.254/Packer/qemu-ga.64bit.msi"
+if (!(Test-Path "C:\Program Files (x86)\qemu-ga" )) {
+    Write-Host "Downloading $qga_download_url"
+    $msiFile = "C:\Windows\Temp\qemu-ga.64bit.msi"
+    $targetdit = "C:\Program Files\qemu-ga"
+    ( New-Object System.Net.WebClient).DownloadFile( $qga_download_url , "$msiFile" )
+    $arguments = @(
+        "/i"
+        "`"$msiFile`""
+        "/qn"
+        "/norestart"
+        "ALLUSERS=1"
+        "TARGETDIR=`"$targetdit`""
+    )
+    Write-Host "Installing $msiFile....."
+    $process = Start-Process -FilePath msiexec.exe -ArgumentList $arguments -Wait -PassThru
+    if ($process.ExitCode -eq 0){
+        Write-Host "$msiFile has been successfully installed"
+    } else {
+        Write-Host "installer exit code  $($process.ExitCode) for file  $($msifile)"
+    }
+    Start-Service "QEMU Guest Agent"
+}
