07d6d1468c6923e964cdbe3adf5febb63974554f
David <dawei@yunshan.net.cn>
2015-12-25 13:11:58 +0800

    fix opensuse42.1 install failed.

--- opensuse_13.2_kvm_livecloud/scripts/custom_kvm.sh
+++ opensuse_13.2_kvm_livecloud/scripts/custom_kvm.sh
@@ -34,6 +34,10 @@ rm -rf /bin/sh && ln -s /bin/bash /bin/sh
 # wget qemu_ga
 cd /usr/bin && wget $QEMU_GA && mv qemu-ga.suse13 qemu-ga && chmod +x qemu-ga
 
+# remove ip and mac address
+rm -fr /etc/udev/rules.d/70-persistent-net.rules
+rm -fr /etc/sysconfig/network/ifcfg-eth0
+
 # enable tty console
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
 sed -i -e 's#GRUB_CMDLINE_LINUX=""#GRUB_CMDLINE_LINUX="console=ttyS0 console=ttyS0,115200n8"#' \
