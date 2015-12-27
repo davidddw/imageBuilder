78024461769ae56321ddabf36458b71e253fa2fd
David <dawei@yunshan.net.cn>
2015-12-27 11:51:11 +0800

    add ubuntu15.10 support.
    Signed-off-by: David <dawei@yunshan.net.cn>

--- ubuntu_15.10_kvm_livecloud/scripts/base.sh
+++ ubuntu_15.10_kvm_livecloud/scripts/base.sh
@@ -1,14 +1,14 @@
 cat <<'EOF' > /etc/apt/sources.list
-deb http://cn.archive.ubuntu.com/ubuntu/ vivid main restricted universe multiverse
-deb http://cn.archive.ubuntu.com/ubuntu/ vivid-security main restricted universe multiverse
-deb http://cn.archive.ubuntu.com/ubuntu/ vivid-updates main restricted universe multiverse
-deb http://cn.archive.ubuntu.com/ubuntu/ vivid-proposed main restricted universe multiverse
-deb http://cn.archive.ubuntu.com/ubuntu/ vivid-backports main restricted universe multiverse
-deb-src http://cn.archive.ubuntu.com/ubuntu/ vivid main restricted universe multiverse
-deb-src http://cn.archive.ubuntu.com/ubuntu/ vivid-security main restricted universe multiverse
-deb-src http://cn.archive.ubuntu.com/ubuntu/ vivid-updates main restricted universe multiverse
-deb-src http://cn.archive.ubuntu.com/ubuntu/ vivid-proposed main restricted universe multiverse
-deb-src http://cn.archive.ubuntu.com/ubuntu/ vivid-backports main restricted universe multiverse
+deb http://cn.archive.ubuntu.com/ubuntu/ wily main restricted universe multiverse
+deb http://cn.archive.ubuntu.com/ubuntu/ wily-security main restricted universe multiverse
+deb http://cn.archive.ubuntu.com/ubuntu/ wily-updates main restricted universe multiverse
+deb http://cn.archive.ubuntu.com/ubuntu/ wily-proposed main restricted universe multiverse
+deb http://cn.archive.ubuntu.com/ubuntu/ wily-backports main restricted universe multiverse
+deb-src http://cn.archive.ubuntu.com/ubuntu/ wily main restricted universe multiverse
+deb-src http://cn.archive.ubuntu.com/ubuntu/ wily-security main restricted universe multiverse
+deb-src http://cn.archive.ubuntu.com/ubuntu/ wily-updates main restricted universe multiverse
+deb-src http://cn.archive.ubuntu.com/ubuntu/ wily-proposed main restricted universe multiverse
+deb-src http://cn.archive.ubuntu.com/ubuntu/ wily-backports main restricted universe multiverse
 EOF
 
 apt-get update
