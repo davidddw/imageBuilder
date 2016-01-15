c2894e3e90fcaa1f9e3812df5f6915cf319daa02
David <dawei@yunshan.net.cn>
2016-01-15 11:20:03 +0800

    add centos6.7 & 7.2 i386 support.
    Signed-off-by: David <dawei@yunshan.net.cn>

---
+++ centos_7.2_i386_kvm_livecloud/build_kvm.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -x
+
+: ${BUILD_VERSION:="v$(date +'%Y%m%d%H%M%S')"}
+: ${BUILD_NAME:="CentOS_7.2-i386"}
+: ${VM_NAME:="centos_7.2"}
+
+export BUILD_NAME
+export VM_NAME
+export BUILD_VERSION
+
+PWD=`pwd`
+FILENAME=${VM_NAME}
+PACKER=/usr/bin/packer
+
+if [ -e "${PWD}/disk" ];
+then
+    rm -rf ${PWD}/disk
+fi
+
+if [ ! -e "${PWD}/final_images" ];
+then
+    mkdir -pv ${PWD}/final_images
+fi
+
+$PACKER build template_kvm.json
+
+cd disk
+qemu-img convert -c -O qcow2 $FILENAME ${BUILD_NAME}-${BUILD_VERSION}.qcow2
+cd -
+
+mv ${PWD}/disk/${BUILD_NAME}-${BUILD_VERSION}.qcow2 ${PWD}/final_images
+rm -rf ${PWD}/disk
+echo "==> Generate files:"
+find ${PWD}/final_images -type f -printf "==> %f\n"
+
+echo "Done"
---
+++ centos_7.2_i386_kvm_livecloud/scripts/custom_kvm.sh
@@ -0,0 +1,39 @@
+# add custom script in here
+
+VM_INIT='http://172.16.2.254/Packer/qga/vm_init.sh'
+QEMU_GA='http://172.16.2.254/Packer/qga/qemu-ga.el7'
+
+cat <<'EOF' > /usr/lib/systemd/system/qemu-guest-agent.service
+[Unit]
+Description=QEMU Guest Agent
+BindsTo=dev-virtio\x2dports-org.qemu.guest_agent.0.device
+After=dev-virtio\x2dports-org.qemu.guest_agent.0.device
+
+[Service]
+UMask=0077
+ExecStart=/usr/bin/qemu-ga \
+  --method=virtio-serial \
+  --path=/dev/virtio-ports/org.qemu.guest_agent.0 \
+  --blacklist=guest-file-open,guest-file-close,guest-file-read,guest-file-write,guest-file-seek,guest-file-flush
+StandardError=syslog
+Restart=always
+RestartSec=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable qemu-guest-agent.service
+mkdir -p /usr/var/run/
+
+# wget vm_init
+cd /etc/ && wget $VM_INIT && chmod +x vm_init.sh
+# wget qemu_ga
+cd /usr/bin && wget $QEMU_GA && mv qemu-ga.el7 qemu-ga && chmod +x qemu-ga
+
+# enable tty console
+sed -i 's#quiet#quiet console=ttyS0#' /etc/default/grub
+grub2-mkconfig -o /boot/grub2/grub.cfg
+
+# remove ip and mac address
+rm -fr /etc/udev/rules.d/70-persistent-net.rules
+rm -fr /etc/sysconfig/network-scripts/ifcfg-eth0
