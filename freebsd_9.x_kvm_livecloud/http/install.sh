70ad0e24d72554b1bd83ad63063393329f5e06d6
David <dawei@yunshan.net.cn>
2016-02-18 22:49:30 +0800

    fix build freebsd failed.
    Signed-off-by: David <dawei@yunshan.net.cn>

--- freebsd_9.x_kvm_livecloud/http/install.sh
+++ freebsd_9.x_kvm_livecloud/http/install.sh
@@ -19,6 +19,7 @@ EOF
 
 env ASSUME_ALWAYS_YES=1 pkg bootstrap
 pkg update
+pkg install -y sudo
 pkg install -y bash
 pkg install -y curl
 pkg install -y vim, wget
@@ -27,5 +28,8 @@ pkg install -y ca_root_nss
 ln -sf /usr/local/share/certs/ca-root-nss.crt /etc/ssl/cert.pem
 
 echo -n 'yunshan3302' | pw usermod root -h 0
+pw groupadd -n vagrant -g 1000
+echo -n 'vagrant' | pw useradd -n vagrant -u 1000 -s /usr/local/bin/bash -m -d /home/vagrant/ -G vagrant -h 0
+echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >> /usr/local/etc/sudoers
 
 reboot
